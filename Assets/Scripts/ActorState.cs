223533d3b7598fd18af501e2484cf9ff8517eccf
dyronzha <dyron85511@gmail.com>
2019-12-22 19:18:20 +0800

    玩家架構(未完成)

---
+++ Assets/Scripts/ActorState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BloodBond {
+    public class ActorState
+    {
+        int stateStep;
+        float time;
+        float maxTime;
+
+        public void Update() { 
+        
+        }
+    }
+
+    public class PlayerState : ActorState { 
+    
+    }
+
+    public class EnemyState : ActorState { 
+        
+    }
+
+}
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BloodBond {
+    public class Player : MonoBehaviour
+    {
+        PlayerState state;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
+
