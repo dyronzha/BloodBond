903ed62ca7be89afa5f21934abd4c831ee9d5a5c
dyronzha <dyron85511@gmail.com>
2019-12-22 17:14:45 +0800

    Rewired

---
+++ Assets/Scripts/ActorValue.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorValue
+{
+    int hp;
+    public int HP{
+        get { return hp; }
+        set { hp = value; }
+    }
+
+    int damage;
+    public int Damage
+    {
+        get { return damage; }
+        set { damage = value; }
+    }
+
+    int moveSpeed;
+    public int MoveSpeed {
+        get { return moveSpeed; }
+        set { moveSpeed = value; }
+    }
+
+}
+
+public class PlayerValue : ActorValue 
+{ 
+
+}
+
+public class EnemyValue : ActorValue
+{
+
+}
+
