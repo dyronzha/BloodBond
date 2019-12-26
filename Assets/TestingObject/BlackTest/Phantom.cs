a8dfe68055783fd9b73717c1ef53a4c8f2ad3c37
moraloyuxing <s110319023@stu.ntue.edu.tw>
2019-12-26 18:29:27 +0800

    1226_幻影消除大失敗

--- Assets/TestingObject/BlackTest/Phantom.cs
+++ Assets/TestingObject/BlackTest/Phantom.cs
@@ -7,29 +7,38 @@ public class Phantom : MonoBehaviour{
 
     float _Rate = 40.0f;
     float Emission_Moment;
-    float AliveTime = 0.3f;
-    float DisappearTime = 0.9f;
-    int Emission_State = 0;
+    float AliveTime = 1f;
+    float DisappearTime = 2f;
+    int Emission_State;
+
+    private void Start()
+    {
+        Emission_State = 0;
+    }
 
     void Update(){
-
+        Debug.Log("Emission_State = " + Emission_State + "in update");
+        Debug.Log(Emission_State);
         if (Emission_State == 1 && Time.time > Emission_Moment + AliveTime) {
+            Debug.Log("line19");
             GetComponent<VisualEffect>().SetInt("Number_of_Particles", 0);
             Emission_State = 2;
         }
 
         if (Emission_State == 2 && Time.time > Emission_Moment + DisappearTime) {
-            Emission_State = 0;
+            Debug.Log("line25");
+            //Emission_State = 0;
             Destroy(gameObject);
         }
     }
 
     //都有改到，但Update被重置(Rate,EmissionState)
     public void PhantomEmission() {
-        GetComponent<VisualEffect>().SetInt("Number_of_Particles", 5000);
+        GetComponent<VisualEffect>().SetInt("Number_of_Particles", 3000);
         GetComponent<VisualEffect>().playRate = _Rate;
         Emission_Moment = Time.time;
         Emission_State = 1;
+        Debug.Log("Emission_State = " + Emission_State + "in function");
     }
 
 }
