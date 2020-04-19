e42e7f8791eacd79c29471653faf965b578fb1af
moraloyuxing <s110319023@stu.ntue.edu.tw>
2020-04-20 04:03:05 +0800

    0420_魘粉化1st

---
+++ Assets/TestingObject/BlackTest/Nightnare_powder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class Nightnare_powder : MonoBehaviour{
+
+    float Trigger_Moment = 0.0f;
+    bool Powder = false;
+    SkinnedMeshRenderer _skin;
+    VisualEffect VFX_Powder;
+
+    void Start(){
+        _skin = transform.GetChild(0).GetComponent<SkinnedMeshRenderer>();
+        VFX_Powder = transform.GetChild(1).GetComponent<VisualEffect>();
+        VFX_Powder.Stop();
+    }
+
+
+    void Update(){
+        if (Powder == true && Time.time > Trigger_Moment + 2.0f) {
+            Powder = false;
+            _skin.enabled = false;
+            VFX_Powder.Play();
+        }
+    }
+
+    public void Become_Powder() {
+        Trigger_Moment = Time.time;
+        Powder = true;
+    }
+
+}
