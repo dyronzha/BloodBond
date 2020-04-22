20c5c3f5c185df97568f184c5432653d8369f714
moraloyuxing <s110319023@stu.ntue.edu.tw>
2020-04-22 22:07:11 +0800

    0422_對話系統導入

---
+++ Assets/TestingObject/BlackTest/Scripts/Dialogue/DialogueBoxSetting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueBoxSetting : MonoBehaviour{
+
+    //public TestPlayer _testplayer;
+    public DialogueManager _dialoguemanager;
+
+    public void LoadEmotion() {
+        _dialoguemanager.CheckActorEmotion();
+    }
+
+    public void LoadDialogue() {
+        _dialoguemanager.DisplayNextSentence();
+    }
+
+    public void EndorChange() {
+        //_testplayer.SwitchDialogue(false);
+        _dialoguemanager.DuringDialogue = false;
+    }
+
+}
---
+++ Assets/TestingObject/BlackTest/Scripts/Dialogue/MainEventIO.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MainEventIO : MonoBehaviour{
+
+    string ReadPath_CSV;
+    int RowCount = 0;
+    public Queue<string> AllActorNames = new Queue<string>();
+    public Queue<string> AllEmotions = new Queue<string>();
+    public Queue<string> AllEventSentences = new Queue<string>();
+    public EventManager _eventmanager;
+
+    void Awake(){
+        AllActorNames.Clear();
+        AllEmotions.Clear();
+        AllEventSentences.Clear();
+        ReadPath_CSV = Application.streamingAssetsPath + "/1_MainEvent.csv";//之後用switch case選擇章節載入
+        ReadCSVFile(ReadPath_CSV);
+    }
+
+    void ReadCSVFile(string filePath){
+        StreamReader sReader = new StreamReader(filePath);
+        bool EndofFile = false;
+
+        while (!EndofFile){
+            string line = sReader.ReadLine();
+            if (line == null){
+                EndofFile = true;
+                break;
+            }
+            if (RowCount != 0){
+                var data_values = line.Split(',');
+                AllActorNames.Enqueue(data_values[0]);
+                AllEmotions.Enqueue(data_values[1]);
+                AllEventSentences.Enqueue(data_values[2]);
+            }
+            RowCount++;
+        }
+    }
+
+    public void TriggerMainEvent() {
+        while (AllActorNames.Count != 0 && AllActorNames.Peek() != "") {
+            _eventmanager.PushEventToQueue(AllActorNames.Dequeue(), AllEmotions.Dequeue(), AllEventSentences.Dequeue());
+        }
+        if (AllActorNames.Count !=0 && AllActorNames.Peek() == "") {
+            AllActorNames.Dequeue();
+            AllEmotions.Dequeue();
+            AllEventSentences.Dequeue();
+        }
+    }
+}
