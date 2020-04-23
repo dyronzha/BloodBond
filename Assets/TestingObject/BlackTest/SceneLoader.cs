50aeb84528ba44a9ac3a48fa4d14996549b78548
moraloyuxing <s110319023@stu.ntue.edu.tw>
2020-04-24 02:06:21 +0800

    0424_OP

--- Assets/TestingObject/BlackTest/SceneLoader.cs
+++ Assets/TestingObject/BlackTest/SceneLoader.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour{
 
     int index;
+    //public Animator BlackPanel;
 
     void Start(){
         index = SceneManager.GetActiveScene().buildIndex;
